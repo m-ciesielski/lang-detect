c78e649ed53fd5a4679887f8656261e9c1c5e275
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2017-10-25 21:08:07 +0200

    Shuffle training data, model and tests improvements

--- lang_detect_eng_train.py
+++ lang_detect_eng_train.py
@@ -8,7 +8,7 @@ from keras.models import Sequential
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
 
-# TODO: reduce bias to positive output, use n-grams instead of bag of words approach
+# TODO: use n-grams instead of bag of words approach?
 
 SEED = 7
 numpy.random.seed(SEED)
@@ -28,12 +28,13 @@ for lang in languages:
 
 # Use pandas for further processing
 dataset = pandas.DataFrame(data=dataset)
+# Shuffle dataset
+dataset = dataset.sample(frac=1)
 print(dataset[:10])
 
 # Tokenize texts
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(dataset['text'])
-# print(dataset['text'][:10])
 preprocessed_texts = tokenizer.texts_to_sequences(dataset['text'])
 
 sentences_count = len(preprocessed_texts)
@@ -56,23 +57,21 @@ with open('lang-detect-eng-tokenizer.pkl', 'wb') as tokenizer_file:
 # Map languages for binary classification
 classification_map = {'en': 1, 'es': 0, 'fr': 0}
 preprocessed_labels = [classification_map[lang] for lang in dataset['lang']]
-# print(preprocessed_labels[10:])
 
 # Create model
 model = Sequential()
-model.add(Embedding(len(word_index) + 1, 8, input_length=max_sentence_length))
+model.add(Embedding(len(word_index) + 1, 64, input_length=max_sentence_length))
 # TODO: SpatialDropout?
-model.add(Convolution1D(filters=16, kernel_size=3, padding='valid', activation='relu'))
+model.add(Convolution1D(filters=64, kernel_size=3, padding='valid', activation='relu'))
 model.add(MaxPooling1D(3))
 model.add(Dropout(0.1))
 model.add(Flatten())
-model.add(Dense(16, activation='relu'))
+model.add(Dense(32, activation='relu'))
 model.add(Dropout(0.1))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 
 # Train model
-#preprocessed_texts = numpy.asarray(preprocessed_texts)
 print(preprocessed_texts[:10])
 preprocessed_labels = numpy.asarray(preprocessed_labels)
 print(preprocessed_labels[:10])
@@ -81,13 +80,18 @@ input_dim = preprocessed_texts.shape[1]
 print('input dim: {}'.format(input_dim))
 
 model.fit(x=preprocessed_texts, y=preprocessed_labels,
-          validation_split=0.4, epochs=6, batch_size=32, verbose=2)
+          validation_split=0.4, epochs=5, batch_size=32, verbose=2)
+# Epoch 5/5
+# 22s - loss: 0.0118 - acc: 0.9966 - val_loss: 0.1248 - val_acc: 0.9656
 
-test_sentences = ['Ich bin Hitler!',
+model.save('lang-detect-eng-model.h5')
+
+test_sentences = ['Ich bin Schmetterling!',
                   'I like trains',
                   'How it is going? Well',
                   'Omelette du fromage',
                   'Grzegorz Brzeczyszczykiewicz',
+                  'The more idiomatic way to do this with pandas is to use the .sample method of your dataframe, i.e.',
                   'Chocolate milk is so much better through a straw. I lack said straw',
                   'Pobrać fotokod biletu z linku wysłanego do Ciebie SMS-em - wystarczy połączenie z Internetem (stawka za połączenie zależy od Twojej taryfy).',
                   'Abonnez-vous à la chaine You Tube pour ne manquer aucune vidéo : cliquez ici. Téléchargez le fichier MP3 ici. Téléchargez le fichier PDF ici.']
@@ -99,3 +103,18 @@ predictions = model.predict(preprocessed_test_sentences)
 for i, p in enumerate(predictions):
     print(test_sentences[i])
     print(p)
+
+
+tweet_dataset_path = 'Twitter-Test-Dataset/tweets_1.csv'
+try:
+    tweet_dataset = pandas.read_csv(tweet_dataset_path, delimiter=' ', quotechar='|')
+    preprocessed_tweets = tokenizer.texts_to_sequences(tweet_dataset['Text'])
+    preprocessed_tweets = sequence.pad_sequences(preprocessed_tweets, maxlen=max_sentence_length)
+
+    predictions = model.predict(preprocessed_tweets, verbose=2)
+    for i, p in enumerate(predictions):
+        if p < 0.5:
+            print(tweet_dataset['Text'][i])
+            print(p)
+except Exception:
+    print('Twitter dataset {} not found.'.format(tweet_dataset_path))
